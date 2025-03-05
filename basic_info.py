99fd20a3e9407646866fa8ed23abef53e2f3b082
realandrey <andrewbarulin@gmail.com>
2025-03-05 21:22:14 +0300

    make tasks 1

---
+++ basic_info.py
@@ -0,0 +1,9 @@
+name = input("Введите ваше имя: ")
+profession = input("Введите вашу профессию: ")
+print(f"Привет, {name}! Я знаю что ты {profession}.")
+
+qwestion = str(input("Ты знаешь что такое переменная?: "))
+if qwestion == "Да":
+    print("Молодец")
+else:
+    print("Иди подучи")
