88823edb585b229b825f2f09b98efda4a40a6c2b
realandrey <andrewbarulin@gmail.com>
2025-03-12 21:16:37 +0300

    make tasks 1

---
+++ rectangle_area.py
@@ -0,0 +1,9 @@
+def rectangle_area(a, b):
+    square = a * b
+    print(f"Площадь прямоугольника с длиной {a} и шириной {b} равна {square}.")
+
+
+a = int(input("Введите длину: "))
+b = int(input("Введите ширину: "))
+
+rectangle_area(a, b)
