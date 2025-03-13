2f474ec8d430fdca26f8c422f28dbd622df74bdf
realandrey <andrewbarulin@gmail.com>
2025-03-13 20:57:38 +0300

    make tasks 1

--- power_of.py
+++ power_of.py
@@ -1,11 +1,11 @@
-def power_of(a, b):
-    a = number
-    b = exponent
+def power_of(a, b=2):
+    number = a
+    exponent = b
     oper = number ** exponent
 
-    if b == int:
-        print(f"Число {number} в степени {exponent} равно {oper}.")
-    else:
+    if b == 2:
         print(f"Квадрат числа {a} равен {oper}.")
+    else:
+        print(f"Число {number} в степени {exponent} равно {oper}.")
 
-power_of(3, 2)
+power_of(3, 3)
