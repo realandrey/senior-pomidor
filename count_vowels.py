43c5f27031d05b05eb4390556077c2bc6bb2961a
realandrey <andrewbarulin@gmail.com>
2025-03-14 20:42:19 +0300

    make find_min

---
+++ count_vowels.py
@@ -0,0 +1,10 @@
+def count_vowels(string):
+    vowels = "aeiouAEIOU"  # Гласные буквы
+    count = 0
+    for i in string:
+        if i in vowels:
+            count += 1
+    print(f"Количество гласных в строке '{string}': {count}")
+    return count
+
+count_vowels("Hello World")
