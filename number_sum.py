04e6cea534159a72813f889742d64c6e1f09987e
realandrey <andrewbarulin@gmail.com>
2025-03-14 23:22:56 +0300

    fix number_sum

--- number_sum.py
+++ number_sum.py
@@ -1,12 +1,14 @@
-def math(initial, max_number):
-    initial = int(input("Введите число: "))
-    total = 0
-    while initial < max_number:
-        max_number += 1
-    print(initial)
-    total += initial  # Добавляем число к сумме
-    initial += 1
+def number_sum():
+    n = int(input("Введите число: "))
 
+    total = 0
+    numbers = ""
+
+    for i in range(1, n + 1):
+        numbers += str(i) + " "  # Добавляем число в строку
+        total += i  # Прибавляем число к сумме
+
+    print("Числа:", numbers.strip())  # strip() убирает лишний пробел в конце
     print("Сумма чисел:", total)
 
-math(5,int("n"))
+number_sum()
