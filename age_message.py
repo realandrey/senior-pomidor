0b3d362c575e28be807ff782d2e58ee04676745e
realandrey <andrewbarulin@gmail.com>
2025-03-14 23:25:38 +0300

    fix check_age

--- age_message.py
+++ age_message.py
@@ -2,7 +2,7 @@ def check_age(age):
     year = int(input("Введите год вашего рождения: "))
     age = 2025 - year
     print(f"Ваш возраст: {age}")
-    if age <= 18:
+    if age < 18:
         print("Вы еще молоды, учеба — ваш путь!")
     elif 18 <= age <= 66:
         print("Отличный возраст для карьерного роста!")
