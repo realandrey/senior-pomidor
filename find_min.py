14b0dfb6462691303b66718a8b9d9c06b722481c
realandrey <andrewbarulin@gmail.com>
2025-03-14 20:31:09 +0300

    make find_min

--- find_min.py
+++ find_min.py
@@ -1,1 +1,9 @@
 def find_min(numbers):
+    minmum = numbers[0]
+    for i in numbers:
+        if i < minmum:
+            minmum = i
+    print(f"Минимальное число в списке:", {minmum})
+    return minmum
+
+find_min([3, 10, 4, 3, 5])
