4a5e5add72a86438f6d0ddd7c9d1b8019591e81b
realandrey <andrewbarulin@gmail.com>
2025-03-17 23:49:19 +0300

    did merge_lists

--- lists/merge_lists.py
+++ lists/merge_lists.py
@@ -2,4 +2,4 @@ def merge_lists(list1, list2):
     listmerged = list1 + list2
     return (list(set(listmerged)))
 
-print(merge_lists([1, 2, 3], [3, 4, 5]))  # [1, 2, 3, 4, 5]
+print(merge_lists([1, 2, 3, 4], [3, 4, 5]))  # [1, 2, 3, 4, 5]
