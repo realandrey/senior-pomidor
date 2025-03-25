0ec579adc1b80680fcdc04d93cd2941e64c62951
realandrey <andrewbarulin@gmail.com>
2025-03-25 22:06:41 +0300

    did test 1

--- tests/test_pytest_less.py
+++ tests/test_pytest_less.py
@@ -1,6 +1,6 @@
 # test_pytest_less.py - это имя модуля, обратите внимание, что начинается с test_
 
-from testfile import add  # здесь мы импортируем функцию add в текущий модуль
+from tests.testfile import add  # здесь мы импортируем функцию add в текущий модуль
 
 def test_add():
     assert add(2, 3) == 5
