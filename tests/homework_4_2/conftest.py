e95f6f54fff9792135bc2e011eb45d3cc93e111b
realandrey <andrewbarulin@gmail.com>
2025-04-22 22:19:16 +0300

    add new directory,  fixture in conftest and api homework

---
+++ tests/homework_4_2/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+
+from constant import HEADERS, BASE_URL
+
+@pytest.fixture(scope="session")
+def auth_session():
+    """Создаёт сессию с авторизацией и возвращает объект сессии."""
+    session = requests.session()
+    session.headers.update(HEADERS)
+
+    auth_response = session.post(
+        f"{BASE_URL}/api/v1/login/access-token",
+        json = {"username" : "real_andreyka@mail.ru", "password" : "Test@123"}
+    )
+    assert auth_response.status_code == 200, "Ошибка авторизации, статус код не 200"
+    token = auth_response.json().get('token')
+    assert token is not None, "Токен не найден в ответе"
+
+    session.headers.update({'Cookie':f"token={token}"})
+    return session
---
+++ tests/homework_4_2/constant.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://dashboard.pomidor-stage.ru"
+HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
