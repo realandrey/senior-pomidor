5e95c2dbea168640666d20d351c454c0fe271b1c
realandrey <andrewbarulin@gmail.com>
2025-04-23 22:09:07 +0300

    add put test

--- tests/homework_4_2/test_items.py
+++ tests/homework_4_2/test_items.py
@@ -1,5 +1,5 @@
 import pytest
-from constants import BASE_URL
+from constants import BASE_URL, AUTH_HEADERS, API_HEADERS, AUTH_DATA
 
 
 class TestItems:
@@ -7,14 +7,14 @@ class TestItems:
 
     def test_create_item(self, item_data, auth_session):
         response = auth_session.post(self.endpoint, json=item_data)
-        assert response.status_code in (200, 201), f"Response: {response.status_code}, {response.text}"
+        assert response.status_code in (200, 201), "Элемент не создан"
 
         data = response.json()
         item_id = data.get("id")
         assert item_id is not None
         assert data.get("title") == item_data["title"]
 
-        self.created_item_id = item_id
+        TestItems.created_item_id = item_id
 
     def test_get_items(self, auth_session):
         response = auth_session.get(self.endpoint)
@@ -25,15 +25,17 @@ class TestItems:
         assert isinstance(data["data"], list), "'data' is not a list"
         assert isinstance(data.get("count"), int), "'count' should be integer"
 
-    def test_get_items_filter
+    def test_update_item(self,auth_session):
+        assert hasattr(self.__class__, "created_item_id"), "Нет созданного item_id"
+        item_id = self.__class__.created_item_id
+        updated_data = {
+            "title": "Обновленный заголовок",
+            "description": "Новое описание"
+        }
 
+        response = auth_session.put(f"{self.endpoint}{item_id}", json=updated_data)
+        assert response.status_code == 200, "Данные не обновились"
 
-
-
-
-    def test_get_items_sort
-
-
-
-
-    def test_get_items_pagination
+        data = response.json()
+        assert data.get("title") == updated_data["title"]
+        assert data.get("description") == updated_data["description"]
